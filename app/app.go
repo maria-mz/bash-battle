83e1fa6bc1ecf1716264c16e8d5f2c761d8797d8
Maria Zaman <mariazaman2@yahoo.com>
2024-05-23 18:21:01 -0700

    implement main app

---
+++ app/app.go
@@ -0,0 +1,91 @@
+package app
+
+import (
+	"fmt"
+	"log"
+
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/maria-mz/bash-battle-proto/proto"
+	be "github.com/maria-mz/bash-battle/backend"
+	"github.com/maria-mz/bash-battle/context"
+	"github.com/maria-mz/bash-battle/tui"
+)
+
+type App struct {
+	context context.AppContext
+	backend *be.Backend
+	program *tea.Program
+}
+
+func NewApp(host string, port uint16, username string) (*App, error) {
+	app := &App{}
+	app.context = context.AppContext{}
+
+	if err := app.initBackend(host, port); err != nil {
+		return app, err
+	}
+
+	if err := app.loginUser(username); err != nil {
+		return app, err
+	}
+
+	app.program = tea.NewProgram(tui.NewTui(app.context), tea.WithAltScreen())
+
+	return app, nil
+}
+
+func (app *App) StartTui() error {
+	tea.LogToFile("debug.log", "debug")
+
+	if _, err := app.program.Run(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (app *App) Shutdown() {
+	log.Print("shutting down")
+	if app.program != nil {
+		app.program.Quit()
+	}
+
+	if app.backend != nil {
+		app.backend.Shutdown()
+	}
+}
+
+func (app *App) initBackend(host string, port uint16) error {
+	serverAddr := fmt.Sprintf("%s:%d", host, port)
+
+	backend, err := be.NewBackend(serverAddr)
+
+	if err != nil {
+		return err
+	}
+
+	app.backend = backend
+	app.context.ServerAddress = serverAddr
+
+	return nil
+}
+
+func (app *App) loginUser(username string) error {
+	loginRequest := &proto.LoginRequest{
+		Username: username,
+	}
+
+	resp, err := app.backend.Login(loginRequest)
+
+	if err != nil {
+		return fmt.Errorf("failed to login: %s", err)
+	}
+
+	app.context.Username = username
+	app.context.ConnectionStatus = context.IsConnected
+	app.context.GameStatus = context.WaitingForPlayers
+	app.context.PopulateGameConfig(resp.GameConfig)
+	app.context.PopulatePlayerNames(resp.Players)
+
+	return nil
+}
--- go.mod
+++ go.mod
@@ -5,16 +5,14 @@ go 1.22.2
 require (
 	github.com/charmbracelet/bubbles v0.18.0
 	github.com/charmbracelet/bubbletea v0.26.1
-	github.com/charmbracelet/huh v0.3.0
 	github.com/charmbracelet/lipgloss v0.10.1-0.20240506202754-3ee5dcab73cb
-	github.com/maria-mz/bash-battle-proto v0.0.0-20240520003722-71c5eeed5ce5
+	github.com/maria-mz/bash-battle-proto v0.0.0-20240524001703-dddeda814a56
 	google.golang.org/grpc v1.64.0
 )
 
 require (
 	github.com/atotto/clipboard v0.1.4 // indirect
 	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
-	github.com/catppuccin/go v0.2.0 // indirect
 	github.com/charmbracelet/x/exp/term v0.0.0-20240506152644-8135bef4e495 // indirect
 	github.com/erikgeiser/coninput v0.0.0-20211004153227-1c3628e74d0f // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
@@ -26,6 +24,7 @@ require (
 	github.com/muesli/reflow v0.3.0 // indirect
 	github.com/muesli/termenv v0.15.2 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
+	github.com/sahilm/fuzzy v0.1.1-0.20230530133925-c48e322e2a8f // indirect
 	golang.org/x/net v0.23.0 // indirect
 	golang.org/x/sync v0.7.0 // indirect
 	golang.org/x/sys v0.20.0 // indirect
