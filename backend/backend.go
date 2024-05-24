74994b659df460b3a9c5bc7262b6be9a45e6eab4
Maria Zaman <mariazaman2@yahoo.com>
2024-05-23 18:19:33 -0700

    create backend with server addr, let it handle grpc stuff

--- backend/backend.go
+++ backend/backend.go
@@ -2,9 +2,12 @@ package backend
 
 import (
 	"context"
+	"fmt"
 	"log/slog"
 
-	pb "github.com/maria-mz/bash-battle-proto/proto"
+	"github.com/maria-mz/bash-battle-proto/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/grpc/metadata"
 )
 
@@ -12,17 +15,42 @@ type ServerEvent interface{}
 
 type Backend struct {
 	token            string
-	grpcClient       pb.BashBattleClient
-	stream           pb.BashBattle_StreamGameClient
+	conn             *grpc.ClientConn
+	grpcClient       proto.BashBattleClient
+	stream           proto.BashBattle_StreamClient // NOTE: this is nil by default
 	ServerEventsChan chan ServerEvent
 	StreamEnded      chan bool
 }
 
-func NewBackend(grpcClient pb.BashBattleClient) *Backend {
+func NewBackend(serverAddr string) (*Backend, error) {
+	conn, err := grpc.NewClient(
+		serverAddr,
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
+
+	if err != nil {
+		conn.Close()
+		return nil, err
+	}
+
+	client := proto.NewBashBattleClient(conn)
+
 	return &Backend{
-		grpcClient:       grpcClient,
+		conn:             conn,
+		grpcClient:       client,
 		ServerEventsChan: make(chan ServerEvent),
 		StreamEnded:      make(chan bool),
+	}, nil
+}
+
+func (be *Backend) Shutdown() {
+	// TODO: close channels (test)
+	if be.conn != nil {
+		be.conn.Close()
+	}
+
+	if be.stream != nil {
+		be.stream.CloseSend()
 	}
 }
 
@@ -34,52 +62,20 @@ func (be *Backend) getAuthContext() context.Context {
 	return ctx
 }
 
-func (be *Backend) Login(in *pb.LoginRequest) (*pb.LoginResponse, error) {
-	resp, err := be.grpcClient.Login(context.Background(), in)
+func (be *Backend) Login(request *proto.LoginRequest) (*proto.LoginResponse, error) {
+	resp, err := be.grpcClient.Login(context.Background(), request)
 
-	if err == nil && resp.ErrorCode == nil {
+	if err == nil && resp.Error == nil {
 		be.token = resp.Token
 	}
 
 	return resp, err
 }
 
-func (be *Backend) CreateGame(in *pb.CreateGameRequest) (*pb.CreateGameResponse, error) {
+func (be *Backend) Stream() error {
 	ctx := be.getAuthContext()
 
-	resp, err := be.grpcClient.CreateGame(ctx, in)
-
-	return resp, err
-}
-
-func (be *Backend) JoinGame(in *pb.JoinGameRequest) (*pb.JoinGameResponse, error) {
-	ctx := be.getAuthContext()
-
-	resp, err := be.grpcClient.JoinGame(ctx, in)
-
-	return resp, err
-}
-
-func (be *Backend) GetGameConfig(in *pb.ConfigRequest) (*pb.ConfigResponse, error) {
-	ctx := be.getAuthContext()
-
-	resp, err := be.grpcClient.GetGameConfig(ctx, in)
-
-	return resp, err
-}
-
-func (be *Backend) LeaveGame() error {
-	ctx := be.getAuthContext()
-
-	_, err := be.grpcClient.LeaveGame(ctx, &pb.EmptyMessage{})
-
-	return err
-}
-
-func (be *Backend) StreamGame() error {
-	ctx := be.getAuthContext()
-
-	stream, err := be.grpcClient.StreamGame(ctx, &pb.EmptyMessage{})
+	stream, err := be.grpcClient.Stream(ctx)
 
 	if err != nil {
 		return err
@@ -98,10 +94,12 @@ func (be *Backend) recvStream() {
 
 		if err != nil {
 			slog.Error("failed to receive from game stream")
-			be.StreamEnded <- true
+			// be.StreamEnded <- true
 			return
 		}
 
-		be.ServerEventsChan <- event
+		slog.Info(fmt.Sprintf("received event!!! %+v", event))
+
+		// be.ServerEventsChan <- event
 	}
 }
